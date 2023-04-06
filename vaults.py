c70d1bcca722781af6c732c152e379ef8728360f
devmegan <devmegan@protonmail.com>
2023-04-06 08:22:37 +0000

    add: views to connect to db and return facilities

--- vaults.py
+++ vaults.py
@@ -1,7 +1,44 @@
-from flask import Flask
+from dotenv import load_dotenv
+import os
+from flask import Flask, jsonify
+import psycopg2
+from tabulate import tabulate
 
 svdb = Flask(__name__)
+
+load_dotenv()
+load_dotenv('.env.local', override=True)
+
+db_config = {
+    "host": os.environ.get('PGRES_HOST'),
+    "database": os.environ.get('DB_NAME'),
+    "user": os.environ.get('DB_USERNAME'),
+    "password": os.environ.get('DB_PASSWORD')
+}
+
 
 @svdb.route("/")
 def index():
     return "<h1>SVDB</h1><p>The Seed Vault Database</p>"
+
+
+@svdb.route("/facilities")
+def facilities():
+    conn = postgres_connect()
+
+    cur = conn.cursor()
+    cur.execute("SELECT * FROM facilities ORDER BY country")
+
+    res = cur.fetchall()
+
+    if res:
+        headers = ["id", "name", "city", "country"]
+        results = [dict(zip(headers, row)) for row in res]
+        return jsonify({"results": results})
+    else:
+        return jsonify({"results": "No facilities found"})
+
+
+def postgres_connect():
+    conn = psycopg2.connect(**db_config)
+    return conn
